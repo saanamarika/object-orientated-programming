5b69327bf044d9d144376ea9eee86c6b30c4e281
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-11 11:03:26 +0200

    Task-Person Example done

---
+++ object-method/Task-Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    class Person
+    {
+        //Fields (Muuttujat)
+        public string name;
+        private int age;
+
+        //Constructor (Konstruktori)
+        public Person()
+        {
+            name = "unknown";
+            age = 0;
+        }
+        public Person(string Name, int age)
+        {
+            name = Name;
+            this.age = age;
+        }
+        //Methods (Metodit)
+        public void GrowOld()
+        {
+            this.age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {name}\nIkä: {this.age}\nAikuinen: {IsAdult()} ");
+        }
+        public bool IsAdult()
+        {
+            return this.age >= 18;
+
+            //Voit tehdä sen myös näin:
+
+            /*if (this.age >= 18)
+                return true;
+            else
+                return false;*/
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ object-method/Task-Person/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Person personSaana = new Person();
+            personSaana.name = "Saana S.";
+            personSaana.GrowOld();
+            personSaana.PrintPersonInfo();
+
+            Person newPerson = new Person("Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
