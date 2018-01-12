eb96ba7198b8c888327d6051e6769f3373ce6d4e
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-12 09:28:07 +0200

    BookClass done

---
+++ BookClass/Books.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookClass
+{
+    class Books
+    {
+        //Fields
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        //Constructor
+        public Books()
+        {
+            Title = "unknown";
+            Author = "unknown";
+            Id = 0;
+            Price = 0;
+        }
+        public Books(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+        //Methods
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Kirja: {Title}\nKirjailija: {Author}\nId: {Id}\nHinta: {Price}\n");
+        }
+        //Tässä on se vertailu mikä vain kutsutaan pääohjelmassa
+        public string BookPrices(Books books)
+        {
+            if (this.Price < books.Price)
+                return ($"Kirja {this.Title} on edullisempi kuin {books.Title}\n");
+            return ($"Kirja {this.Title} on kalliimpi kuin {books.Title}\n");
+        }
+
+    }
+}
---
+++ BookClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BookClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Books newBook = new Books();
+            newBook.Title = "Kadonneen koodin arvoitus";
+            newBook.Author = "Mäntti Mänttinen";
+            newBook.Id = 1234567;
+            newBook.Price = 39.95;
+            newBook.PrintInfo();
+
+            Books newBook2 = new Books("Koodaus one ou one", "Saanis Sieppaus", 1234, 12.95);
+            newBook2.PrintInfo();
+
+            Books book = new Books("Digimonit ja koodin salaisuus", "Pertti Jaakola", 123456, 29.95);
+            book.PrintInfo();
+
+            Console.WriteLine(newBook.BookPrices(book));
+            Console.WriteLine(newBook2.BookPrices(newBook));
+            Console.WriteLine(newBook2.BookPrices(book));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
