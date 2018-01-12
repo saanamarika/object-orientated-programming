42fc09f279ae15fab60430e8c41daf47766d9943
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-12 10:51:55 +0200

    CarClass done

---
+++ object-method/CarClass/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClass
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public int Speed;
+        //Constructions
+        public Car()
+        {
+            Brand = "";
+            Speed = 0;
+        }
+        //methods
+        public void AskData()
+        {
+            Console.WriteLine("Syötä autosi merkki ja nopeus km/h:");
+            Brand = Console.ReadLine();
+            Speed = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Merkki: {Brand}\nNopeus: {Speed}");
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Annoit autosi merkiksi {Brand} ja nopeudeksi {Speed}km/h\n");
+        }
+        public void Accelerate()
+        {
+            double Speed1 =Speed* 1.5;
+            Console.WriteLine($"Autosi parannettu nopeus olisi: {Speed1}\n");
+        }
+        public void Brake()
+        {
+            double Speed2 = Speed * 0.9;
+            Console.WriteLine($"Autosi hidastettu nopeus olisi: {Speed2}\n");
+        }
+
+    }
+}
---
+++ object-method/CarClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Car car = new Car();
+            car.AskData();
+            car.ShowCarInfo();
+            car.Accelerate();
+            car.Brake();
+
+            Car car2 = new Car();
+            car2.AskData();
+            car2.ShowCarInfo();
+            car2.Accelerate();
+            car2.Brake();
+
+            Console.ReadKey();
+        }
+    }
+}
