05d4f8c6fd923367ebb73e03dcfb51c3a784c4c6
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-12 11:31:59 +0200

    FirmClass beginning

---
+++ object-method/FirmClass/Firm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirmClass
+{
+    class Firm
+    {
+        //Fields
+        public string Title;
+        public string Address;
+        public int Phone;
+        public int Outcome;
+        public int Expense;
+
+        public Firm()
+        {
+            Title = "";
+            Address = "";
+            Phone = 0;
+            Outcome = 0;
+            Expense = 0;
+        }
+        public Firm(string title, string address, int phone, int outcome, int expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Outcome = outcome;
+            Expense = expense;
+        }
+        public Firm(Firm previousNumber)
+        {
+            Title = previousNumber.Title;
+            Address = previousNumber.Address;
+            Phone = previousNumber.Phone;
+            Outcome = previousNumber.Outcome;
+            Expense = previousNumber.Expense;
+        }
+    }
+}
---
+++ object-method/FirmClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirmClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        }
+    }
+}
