f087f865fa4308916541f4c53f487b69d5034a84
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-18 09:00:07 +0200

    firmClass in progress

--- object-method/FirmClass/Firm.cs
+++ object-method/FirmClass/Firm.cs
@@ -9,7 +9,7 @@ namespace FirmClass
         //Fields
         public string Title;
         public string Address;
-        public int Phone;
+        public string Phone;
         public int Outcome;
         public int Expense;
 
@@ -17,11 +17,11 @@ namespace FirmClass
         {
             Title = "";
             Address = "";
-            Phone = 0;
+            Phone = "";
             Outcome = 0;
             Expense = 0;
         }
-        public Firm(string title, string address, int phone, int outcome, int expense)
+        public Firm(string title, string address, string phone, int outcome, int expense)
         {
             Title = title;
             Address = address;
@@ -36,6 +36,29 @@ namespace FirmClass
             Phone = previousNumber.Phone;
             Outcome = previousNumber.Outcome;
             Expense = previousNumber.Expense;
+        }
+        //methods
+       public void printInfo()
+        {
+            Console.WriteLine($"Firma: {Title}\nOsoite: {Address}\nPuhelin: {Phone}\nTulot: {Outcome}\nMenot: {Expense}");
+        }
+        public void howIsGoing()
+        {
+            int win = (Outcome - Expense) / Expense * 100;
+            Console.WriteLine($"Firman voitto%: {win}");
+            if (win < 100)
+                Console.WriteLine("Firmalla menee helevetin huonosti.\n");
+            if (win >= 100 && win < 200&&win>0)
+                Console.WriteLine("Firmalla menee kehnosti.\n");
+            if (win >= 200 && win < 300)
+                Console.WriteLine("Firmalla menee tyydyttävästi.\n");
+            if (win >= 300)
+                Console.WriteLine("Firmalla menee todella hyvin!\n");
+            else
+                Console.WriteLine("Firmalla menee niin huonosti, että koodikin sekoaa...");
+
+
+
         }
     }
 }
--- object-method/FirmClass/Program.cs
+++ object-method/FirmClass/Program.cs
@@ -7,7 +7,15 @@ namespace FirmClass
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Firm firm = new Firm("Kala", "Kalankatu 15", "045 1234 123", 2100000, 500000);
+            firm.printInfo();
+            firm.howIsGoing();
 
+            Firm firm2 = new Firm("Hauki", "Järvikatu 23", "023 2345", 230500, 123000);
+            firm2.printInfo();
+            firm2.howIsGoing();
+
+            Console.ReadKey();
         }
     }
 }
