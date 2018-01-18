e103fb2277d90c0b03a6ed682ce0456e8e83bfac
saanamarika <saana.sieppi@student.saimia.fi>
2018-01-18 10:41:31 +0200

    EmployeeClass done

--- object-method/EmployeeClass/Employee.cs
+++ object-method/EmployeeClass/Employee.cs
@@ -6,5 +6,36 @@ namespace EmployeeClass
 {
     class Employee
     {
+        //fields
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary;
+
+        public Employee()
+        {
+            Name = "";
+            Id = 0;
+            Position = "";
+            Salary = 0;
+        }
+        public Employee(string name,int id,string position,int salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+        //methods
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijä: {Name}\nId: {Id}\nTitteli: {Position}\nKuukausipalkka: {Salary}\n");
+        }
+        public string CompareSalary(Employee employee)
+        {
+            if (this.Salary < employee.Salary)
+                return ($"Henkilön {this.Name} kuukausipalkka on pienempi, kuin henkilön {employee.Name} palkka\n");
+            return ($"Henkilön {this.Name} kuukausipalkka on suurempi, kuin henkilön {employee.Name}\n");
+        }
     }
 }
--- object-method/EmployeeClass/Program.cs
+++ object-method/EmployeeClass/Program.cs
@@ -6,7 +6,25 @@ namespace EmployeeClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Employee employee1 = new Employee("Pahvi Pahvinen", 1234, "The Bossman", 10000);
+            employee1.PrintEmployeeInfo();
+
+            Employee employee2 = new Employee("Karto Kartonki", 1234, "Luuseri", 1500);
+            employee2.PrintEmployeeInfo();
+
+            Console.WriteLine(employee1.CompareSalary(employee2));
+
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Kalle Lipponen",1234,"Välimalli",2500);
+            employees[1] = new Employee("Risto Rasti",1234,"Pomon pomo",15000);
+            employees[2] = new Employee("Teppo Testaaja",1234,"Johtaja",20000);
+
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+
+            Console.ReadKey();
         }
     }
 }
